ba1e7f46ed8eb1b8c83397b6157b85d9a7de2ca0
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2016-10-27 22:52:32 +0100

    Added a generate script

--- README.rst
+++ README.rst
@@ -2,9 +2,5 @@
 
 Do this:
 
-    virtualenv env
-    . env/bin/activate
-    pip install -r requirements.txt
-
-    django-admin.py graph_models -a -o models.png --settings=idm_model.settings --pythonpath=,
+    ./generate.sh
 
---
+++ generate.sh
@@ -0,0 +1,9 @@
+if [ ! -d env ] ; then
+    python3 -m venv env
+    . env/bin/activate
+    pip install -r requirements.txt
+else
+    . env/bin/activate
+fi
+
+django-admin.py graph_models -a -o models.png --settings=idm_model.settings --pythonpath=. --layout=fdp
